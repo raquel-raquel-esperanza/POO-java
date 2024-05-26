f9e6bb78737dd06889e5050955fad1e527f85756
Raquel <raquel.raquel.esperanza@clases.edu.sv>
2024-05-25 21:07:19 -0600

    Clase 1 y 2

---
+++ 02_Variables.java
@@ -0,0 +1,12 @@
+public class  Variables {
+    public static void main(String[] args) {
+        String name = "Rosa";
+        int edad = 30;
+        boolean casada = true;
+        double altura = 1.75;
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + edad);
+        System.out.println("Casado: " + casada);
+        System.out.println("Altura: " + altura);
+    }
+}
