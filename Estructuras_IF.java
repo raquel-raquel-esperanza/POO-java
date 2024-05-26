d9a20fe0ec7e2c4948049ceed9f7c2610bacd944
jandradee <jandradee@outlook.com>
2024-05-26 00:02:44 -0600

    Estructuras IF

---
+++ Estructuras_IF.java
@@ -0,0 +1,22 @@
+public class Estructuras_IF {
+
+    public static void main(String[] args) {
+
+        String name = "Rosa";
+        int edad = 7;
+        boolean casada = true;
+        double altura = 1.75;
+
+        if (edad > 17) {
+            System.out.println("Edad de " + name + " es: " + edad + "; es mayor de edad");
+        } else if (edad <= 17) {
+            System.out.println("Edad de " + name + " es: " + edad + "; es menor de edad");
+        } 
+    }
+
+    public static void ejemlpo2() {
+        
+
+}
+
+        
